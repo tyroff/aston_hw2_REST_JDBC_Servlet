bed241374da644eb77934ce675916a94bc1d3544
Kirill Vinokurov <tyroff@mail.ru>
2024-07-17 15:22:11 +0300

    upd/model: the field of doctor is added into the class Patient.

--- src/main/java/model/Patient.java
+++ src/main/java/model/Patient.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Patient extends People {
     private String job;
+    private Doctor doctor;
 
     public String getJob() {
         return job;
@@ -13,24 +14,33 @@ public class Patient extends People {
         this.job = job;
     }
 
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Patient patient = (Patient) o;
-        return Objects.equals(job, patient.job);
+        return Objects.equals(job, patient.job) && Objects.equals(doctor, patient.doctor);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), job);
+        return Objects.hash(super.hashCode(), job, doctor);
     }
 
     @Override
     public String toString() {
         return "Patient{" +
                 "job='" + job + '\'' +
+                ", doctor=" + doctor +
                 '}';
     }
 }
