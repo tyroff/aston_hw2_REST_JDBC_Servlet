64fea4d58d813f4dd4f7472bf493cfbbebbf72d8
Kirill Vinokurov <tyroff@mail.ru>
2024-07-17 15:11:13 +0300

    add/model: the class of Patient is added

---
+++ src/main/java/model/Patient.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.Objects;
+
+public class Patient extends People {
+    private String job;
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Patient patient = (Patient) o;
+        return Objects.equals(job, patient.job);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), job);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "job='" + job + '\'' +
+                '}';
+    }
+}
