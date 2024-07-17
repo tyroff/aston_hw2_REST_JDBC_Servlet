513f5935f170584c0282363c9ca0bcda38329a38
Kirill Vinokurov <tyroff@mail.ru>
2024-07-17 15:06:30 +0300

    add/model: the class of People is added.

---
+++ src/main/java/model/People.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public abstract class People {
+    private int id;
+    private String lastName, firstName, patronymic;
+    private Date birthday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        People people = (People) o;
+        return id == people.id && Objects.equals(lastName, people.lastName) && Objects.equals(firstName, people.firstName) && Objects.equals(patronymic, people.patronymic) && Objects.equals(birthday, people.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lastName, firstName, patronymic, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
