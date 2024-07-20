d597f503861a20de15cb46cb17b62d40d97c1479
Kirill Vinokurov <tyroff@mail.ru>
2024-07-20 19:08:17 +0300

    upd/model/People: id is created to Long

--- src/main/java/model/People.java
+++ src/main/java/model/People.java
@@ -4,15 +4,15 @@ import java.util.Date;
 import java.util.Objects;
 
 public abstract class People {
-    private int id;
+    private Long id;
     private String lastName, firstName, patronymic;
     private Date birthday;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
