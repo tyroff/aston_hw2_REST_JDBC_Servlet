614a679126ba6911cfd62e55d4845a23a2b90fa5
Kirill Vinokurov <tyroff@mail.ru>
2024-07-20 19:11:04 +0300

    upd/model/Doctor => it is added specialization and clinic.

--- src/main/java/model/Doctor.java
+++ src/main/java/model/Doctor.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class Doctor extends People{
+    private String specialization;
+    private Clinic clinic;
     private List<Patient> patients;
 
     public List<Patient> getPatients() {
@@ -14,24 +16,42 @@ public class Doctor extends People{
         this.patients = patients;
     }
 
+    public Clinic getClinic() {
+        return clinic;
+    }
+
+    public void setClinic(Clinic clinic) {
+        this.clinic = clinic;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Doctor doctor = (Doctor) o;
-        return Objects.equals(patients, doctor.patients);
+        return Objects.equals(specialization, doctor.specialization) && Objects.equals(clinic, doctor.clinic) && Objects.equals(patients, doctor.patients);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), patients);
+        return Objects.hash(super.hashCode(), specialization, clinic, patients);
     }
 
     @Override
     public String toString() {
         return "Doctor{" +
-                "patients=" + patients +
+                "specialization='" + specialization + '\'' +
+                ", clinic=" + clinic +
+                ", patients=" + patients +
                 '}';
     }
 }
