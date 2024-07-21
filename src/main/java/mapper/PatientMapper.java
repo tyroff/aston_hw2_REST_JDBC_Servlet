b72ca7ab661754aa27f841c6d53d7517d27f9dc4
Kirill Vinokurov <tyroff@mail.ru>
2024-07-21 22:06:57 +0300

    add/mapper/PatientMapper => It is created.

---
+++ src/main/java/mapper/PatientMapper.java
@@ -0,0 +1,19 @@
+package mapper;
+
+import dto.PatientDTO;
+import model.Patient;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface PatientMapper {
+    PatientMapper INSTANCE = Mappers.getMapper(PatientMapper.class);
+    PatientDTO patientToDTO(Patient patient);
+    Patient DTOtoPatient(PatientDTO patientDTO);
+    List<PatientDTO> patientsToDTOs(List<Patient> patients);
+
+    //TODO: @Mapping(source = "doctor", target = "doctor") add
+    //TODO: @Mapping(source = "clinic", target = "clinic") add
+}
