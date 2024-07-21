de75385f09598342fd91819938018f76a40348b1
Kirill Vinokurov <tyroff@mail.ru>
2024-07-22 00:01:14 +0300

    add/controller/PatientServlet => The method doGet is created.

--- src/main/java/controller/PatientServlet.java
+++ src/main/java/controller/PatientServlet.java
@@ -1,24 +1,48 @@
 package controller;
 
+import com.google.gson.Gson;
+import dao.PatientDaoImp;
+import dto.PatientDTO;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.PatientService;
+import util.DataPropertiesUtil;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/patients/*")
 public class PatientServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private PatientService patientService;
+    private final Gson gson = new Gson();
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() throws ServletException {
+        this.patientService = new PatientService(new PatientDaoImp(DataPropertiesUtil.getDataSource()));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String pathInfo = req.getPathInfo();
+        try {
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            if (pathInfo == null || pathInfo.equals("/")) {
+                //TODO: allPatient;
+            } else {
+                Long id = Long.parseLong(pathInfo.split("/")[1]);
+                PatientDTO patientDTO = patientService.getPatientById(id);
+                String patientJson = gson.toJson(patientDTO);
+                resp.getWriter().write(patientJson);
+            }
+        } catch (NumberFormatException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid user ID format.");
+        }
     }
 
     @Override
