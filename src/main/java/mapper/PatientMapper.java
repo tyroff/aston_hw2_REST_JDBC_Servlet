b90a4f789098ce6281b2aaeada99d2125a414508
Kirill Vinokurov <tyroff@mail.ru>
2024-07-21 22:51:04 +0300

    upd//mapper/PatientMapper => The Optional is added into patientToDTO

--- src/main/java/mapper/PatientMapper.java
+++ src/main/java/mapper/PatientMapper.java
@@ -6,11 +6,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface PatientMapper {
     PatientMapper INSTANCE = Mappers.getMapper(PatientMapper.class);
-    PatientDTO patientToDTO(Patient patient);
+    PatientDTO patientToDTO(Optional<Patient> patient);
     Patient DTOtoPatient(PatientDTO patientDTO);
     List<PatientDTO> patientsToDTOs(List<Patient> patients);
 
