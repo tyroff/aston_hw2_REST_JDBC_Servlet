3ba88c717366da93b84af06f186a07e941251615
Kirill Vinokurov <tyroff@mail.ru>
2024-07-26 16:30:06 +0300

    add:dto/DoctorDTO => It is added.

---
+++ src/main/java/dto/DoctorDTO.java
@@ -0,0 +1,70 @@
+package dto;
+
+import model.Clinic;
+import model.Patient;
+
+import java.util.List;
+
+public class DoctorDTO {
+    private Long id;
+    private String lastName, firstName, patronymic;
+    private String specialization;
+    private Clinic clinic;
+    private List<Patient> patients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public Clinic getClinic() {
+        return clinic;
+    }
+
+    public void setClinic(Clinic clinic) {
+        this.clinic = clinic;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+}
